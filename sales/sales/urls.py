a10ce7c0654636fa75042fea1ce9adf35c5a2365
Ulfat <80470406+uzhalikov@users.noreply.github.com>
2024-07-18 08:50:32 +0300

    init project

---
+++ sales/sales/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from main import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contacts/', views.contacts, name='contacts'),
+]
